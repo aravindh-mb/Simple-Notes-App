d25fb043d2758eb08029aeeeda0f325d06507c1f
Aravindh <aaravindh23cse@gmail.com>
2023-02-12 14:06:44 +0530

    added material UI

--- src/components/Createnote.jsx
+++ src/components/Createnote.jsx
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react'
-import "../App.css"
-const Createnote = ({onAdd}) => {
-  
-  const [note,setNote] = useState({
-    title:"",
-    content:""
-  })
-
-  const handleChange= (e)=>{
-     const {name ,value} = e.target
-      // console.log(name , value)
-     setNote({
-      ...note,
-      [name]:value
-     })
-     console.log(note)
-
-  }
-  const submitNote =(e)=>{
-        e.preventDefault()
-      
-        note.title !="" && (
-          onAdd(note)
-       )
-      
-       setNote({
-        title:"",
-        content:""
-      })
-  }
-  return (
-    <>
-       <form>
-        <input name='title' value={note.title} onChange={handleChange} placeholder="Title" required/>
-        <textarea name='content' value={note.content} onChange={handleChange} placeholder="content" required/>
-        <button onClick={submitNote}>Add</button>
-       </form>
-    </>
-  )
-}
-
-export default Createnote
-=======
 import React, { useState } from 'react'
 import "../App.css"
 import Button from '@mui/material/Button';
@@ -69,7 +24,7 @@ const Createnote = ({onAdd}) => {
   const submitNote =(e)=>{
         e.preventDefault()
       
-        note.title !="" && (
+        note.title !="" && note.content !="" && (
           onAdd(note)
        )
       
@@ -104,4 +59,3 @@ const Createnote = ({onAdd}) => {
 }
 
 export default Createnote
->>>>>>> ff69c35 (Designed using Material UI)
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
 import React from 'react'
 import "../App.css"
 const Navbar = () => {
   return (
-    <div className="navbar">
-        To-Do App
+    <div className="navbar nabbar-container">
+        To-Do Application
     </div>
   )
 }
 
-=======
-import React from 'react'
-import "../App.css"
-const Navbar = () => {
-  return (
-    <div className="navbar">
-        To-Do App
-    </div>
-  )
-}
-
->>>>>>> ff69c35 (Designed using Material UI)
 export default Navbar
